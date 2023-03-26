70a3e61df691f58e3b9aa215d36d2ebcb374a5ad
Alexander5747 <96681086+Alexander5747@users.noreply.github.com>
2023-03-27 00:15:49 +0300

    1

--- index.js
+++ index.js
@@ -6,9 +6,6 @@ let del = document.querySelector(".del");
 add.addEventListener('click', ()=>{
     div.innerHTML +=`<div class = "wrap">${input.value}</div>`;
 })
-del.addEventListener("click", ()=>{
-    alert("Нажмите на те задачи кторые хотите удалить");
-    div.addEventListener('click', (e)=>{
-        e.target.remove()
-    });
-})
+div.addEventListener('click', (e)=>{
+    e.target.remove()
+});
